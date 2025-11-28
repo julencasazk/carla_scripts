40cdf7bba26b41b2dbe843b7d619da23f17f7985
julencasazk <julencasazk@gmail.com>
2025-11-28 09:00:06 +0100

    before changes

--- control_plant_test_pure_python.py
+++ control_plant_test_pure_python.py
@@ -3,11 +3,6 @@ import matplotlib.pyplot as plt
 import control as ctl
 from PID import PID
 
-# Plant params
-k = 0.156
-wn = 0.396
-zeta = 0.661
-Td = 0.146
 
 # Simulation / sampling
 Ts = 0.01
@@ -15,6 +10,15 @@ T_sim = 80.0
 N_steps = int(T_sim / Ts)
 
 # Continuous-time plant without delay
+
+# Plant from paper, not representative
+'''
+# Params
+k = 0.156
+wn = 0.396
+zeta = 0.661
+Td = 0.146
+
 s = ctl.TransferFunction.s
 G0 = k / (s**2 + 2*zeta*wn*s + wn**2)
 
@@ -25,27 +29,52 @@ H_delay = ctl.tf(num_delay, den_delay)
 # Full continuous plant (with Pade delay), then discretize
 Gc = G0 * H_delay
 Gd = ctl.c2d(Gc, Ts, method='tustin')
+'''
 
 
+# Real discrete plant (2p1z + delay)
+Td = 0.15
+s = ctl.TransferFunction.s
+G0 = (12.68) / (s**2 + 1.076*s + 0.2744)
+num_delay, den_delay = ctl.pade(Td, 1)
+H_delay = ctl.tf(num_delay, den_delay)
+
+# Full continuous plant (with Pade delay), then discretize
+
+Gc = G0 * H_delay
+Gd = ctl.c2d(Gc, Ts, method='tustin')
+    
 print(f"Gd: {Gd}")
 numd, dend = ctl.tfdata(Gd)
 numd = np.squeeze(numd)
 dend = np.squeeze(dend)
 a = dend / dend[0]
 b = numd / dend[0]
+
+
+#a = np.array([1.0, -1.991, 0.991])
+#b = np.array([0.0, 0.001032])
+
 na = len(a) - 1
 nb = len(b) - 1
 
 
 # PID gains
-kp = 20.7837
-ki = 12.8981
-kd = 100.0
+# For CARLA car fitted plant
+kp = 1.05730764
+ki = 1.3201751
+kd = 5.0
+
+# For OLD plant - from paper
+#kp = 1.3354087
+#ki = 0.35239362
+#kd = 4.11808942
+
 N_d = 20
 kb_aw = 1.0
 u_min, u_max = 0.0, 100.0
 
-pid = PID(kp, ki, kd, N_d, Ts, u_min, u_max, kb_aw)
+pid = PID(kp, ki, kd, N_d, Ts, u_min, u_max, kb_aw, der_on_meas=True)
 
 time = np.arange(N_steps) * Ts
 r = np.zeros(N_steps)
@@ -57,7 +86,7 @@ y_hist = np.zeros(na)
 u_hist = np.zeros(nb)
 
 for k_idx in range(N_steps):
-    y_meas = y[k_idx - 1] if k_idx > 0 else 0.0
+    y_meas = y[k_idx-1] if k_idx > 0 else 0.0 
     u_k = pid.step(r[k_idx], y_meas)
     u[k_idx] = u_k
 
