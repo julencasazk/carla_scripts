19405390767659c031e2dc55f643cac1d7030b8f
julencasazk <julencasazk@gmail.com>
2025-12-16 12:31:16 +0100

    check throttle publishing actually applying in CARLA

---
+++ throttle_debug_node.py
@@ -0,0 +1,30 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Float32
+
+class ThrottleDebug(Node):
+    def __init__(self):
+        super().__init__('throttle_debug')
+        self.sub = self.create_subscription(
+            Float32,
+            '/veh_lead/command/throttle',
+            self.cb,
+            10,
+        )
+        self.get_logger().info('ThrottleDebug node started, listening on /veh_lead/command/throttle')
+
+    def cb(self, msg: Float32):
+        self.get_logger().info(f'[ThrottleDebug] received: {msg.data:.2f}')
+
+def main():
+    rclpy.init()
+    node = ThrottleDebug()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
